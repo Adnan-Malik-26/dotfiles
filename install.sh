700b603eeac8ca347c714d74da011d8bad0b2b52
Adnan-Malik-26 <eddymalikadnan@gmail.com>
2023-12-16 00:09:37 +0530

    Added an install script

--- install.sh
+++ install.sh
@@ -4,4 +4,50 @@ CURRENT_DIRECTORY=$(pwd)
 echo "$CURRENT_DIRECTORY"
 #Alacritty
 
-ln -s $CURRENT_DIRECTORY/alacritty/alacritty.yml 
+ln -s $CURRENT_DIRECTORY/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+ln -s $CURRENT_DIRECTORY/bashrc/bashrc ~/.bashrc 
+
+ln -s $CURRENT_DIRECTORY/gtk/gtk-3.0 ~/.config/gtk-3.0/
+
+ln -s $CURRENT_DIRECTORY/gtk/gtk-4.0 ~/.config/gtk-4.0/
+
+ln -s $CURRENT_DIRECTORY/hypr ~/.config/hypr/
+
+ln -s $CURRENT_DIRECTORY/i3 ~/.config/i3/
+
+ln -s $CURRENT_DIRECTORY/liferea ~/.config/liferea/ 
+
+ln -s $CURRENT_DIRECTORY/neofetch ~/.config/neofetch 
+
+ln -s $CURRENT_DIRECTORY/nvim ~/.config/nvim/ 
+
+ln -s $CURRENT_DIRECTORY/polybar ~/.config/polybar/ 
+
+ln -s $CURRENT_DIRECTORY/qBittorrent ~/.config/qBittorrent/ 
+
+ln -s $CURRENT_DIRECTORY/rofi ~/.config/rofi/ 
+
+ln -s $CURRENT_DIRECTORY/starship/starship.toml ~/.config/starship.toml
+
+ln -s $CURRENT_DIRECTORY/waybar ~/.config/waybar/ 
+
+ln -s $CURRENT_DIRECTORY/wlogout ~/.config/wlogout/ 
+
+ln -s $CURRENT_DIRECTORY/scripts/chalac ~/.local/bin/chalac
+
+ln -s $CURRENT_DIRECTORY/scripts/chchrome ~/.local/bin/chchrome
+
+ln -s $CURRENT_DIRECTORY/scripts/chwal ~/.local/bin/chwal
+
+ln -s $CURRENT_DIRECTORY/scripts/getwall ~/.local/bin/getwall
+
+ln -s $CURRENT_DIRECTORY/scripts/gtk.sh ~/.local/bin/gtk.sh
+
+ln -s $CURRENT_DIRECTORY/scripts/rofiwifimenu ~/.local/bin/rofiwifimenu
+
+ln -s $CURRENT_DIRECTORY/scripts/waybar-progress ~/.local/bin/waybar-progress 
+
+ln -s $CURRENT_DIRECTORY/pywaltemplates ~/.config/wal/templates/ 
+
+
