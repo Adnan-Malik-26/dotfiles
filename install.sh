dc6081a8a92f4c3681230b347b866f351ba79c57
Adnan-Malik-26 <eddymalikadnan@gmail.com>
2023-12-18 17:24:52 +0530

    changed install script

--- install.sh
+++ install.sh
@@ -1,58 +1,72 @@
-###INSTALL SCRIPT###
+# ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗         ███████╗ ██████╗██████╗ ██╗██████╗ ████████╗
+# ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║         ██╔════╝██╔════╝██╔══██╗██║██╔══██╗╚══██╔══╝
+# ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║         ███████╗██║     ██████╔╝██║██████╔╝   ██║   
+# ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║         ╚════██║██║     ██╔══██╗██║██╔═══╝    ██║   
+# ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗    ███████║╚██████╗██║  ██║██║██║        ██║   
+# ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝    ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝   
 
+
+#!/bin/bash
+
+# Set the current directory
 CURRENT_DIRECTORY=$(pwd)
-echo "$CURRENT_DIRECTORY"
-#Alacritty
 
-ln -s $CURRENT_DIRECTORY/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+# Function to create symbolic links with error handling
+create_symlink() {
+    source_file=$1
+    target_file=$2
 
-ln -s $CURRENT_DIRECTORY/bashrc/bashrc ~/.bashrc 
+    if [ -e "$target_file" ]; then
+        echo "Symbolic link for $target_file already exists. Skipping..."
+    else
+        ln -s "$source_file" "$target_file"
+        echo "Created symbolic link for $target_file"
+    fi
+}
 
-ln -s $CURRENT_DIRECTORY/gtk/gtk-3.0 ~/.config/gtk-3.0/
+# Ensure that target directories exist
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/gtk-3.0
+mkdir -p ~/.config/gtk-4.0
+mkdir -p ~/.config/hypr
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/liferea
+mkdir -p ~/.config/neofetch
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/polybar
+mkdir -p ~/.config/qBittorrent
+mkdir -p ~/.config/rofi
+mkdir -p ~/.config/waybar
+mkdir -p ~/.config/wlogout
+mkdir -p ~/.config/wal/templates
+mkdir -p ~/.local/bin
 
-ln -s $CURRENT_DIRECTORY/gtk/gtk-4.0 ~/.config/gtk-4.0/
+# Create symbolic links
+create_symlink "$CURRENT_DIRECTORY/alacritty/alacritty.yml" "~/.config/alacritty/alacritty.yml"
+create_symlink "$CURRENT_DIRECTORY/bashrc/bashrc" "~/.bashrc"
+create_symlink "$CURRENT_DIRECTORY/gtk/gtk-3.0" "~/.config/gtk-3.0/"
+create_symlink "$CURRENT_DIRECTORY/gtk/gtk-4.0" "~/.config/gtk-4.0/"
+create_symlink "$CURRENT_DIRECTORY/hypr" "~/.config/hypr/"
+create_symlink "$CURRENT_DIRECTORY/i3" "~/.config/i3/"
+create_symlink "$CURRENT_DIRECTORY/liferea" "~/.config/liferea/"
+create_symlink "$CURRENT_DIRECTORY/neofetch" "~/.config/neofetch"
+create_symlink "$CURRENT_DIRECTORY/nvim" "~/.config/nvim/"
+create_symlink "$CURRENT_DIRECTORY/polybar" "~/.config/polybar/"
+create_symlink "$CURRENT_DIRECTORY/qBittorrent" "~/.config/qBittorrent/"
+create_symlink "$CURRENT_DIRECTORY/rofi" "~/.config/rofi/"
+create_symlink "$CURRENT_DIRECTORY/starship/starship.toml" "~/.config/starship.toml"
+create_symlink "$CURRENT_DIRECTORY/waybar" "~/.config/waybar/"
+create_symlink "$CURRENT_DIRECTORY/wlogout" "~/.config/wlogout/"
+create_symlink "$CURRENT_DIRECTORY/scripts/chalac" "~/.local/bin/chalac"
+create_symlink "$CURRENT_DIRECTORY/scripts/chchrome" "~/.local/bin/chchrome"
+create_symlink "$CURRENT_DIRECTORY/scripts/chwal" "~/.local/bin/chwal"
+create_symlink "$CURRENT_DIRECTORY/scripts/getwall" "~/.local/bin/getwall"
+create_symlink "$CURRENT_DIRECTORY/scripts/gtk.sh" "~/.local/bin/gtk.sh"
+create_symlink "$CURRENT_DIRECTORY/scripts/rofiwifimenu" "~/.local/bin/rofiwifimenu"
+create_symlink "$CURRENT_DIRECTORY/scripts/waybar-progress" "~/.local/bin/waybar-progress"
+create_symlink "$CURRENT_DIRECTORY/pywaltemplates" "~/.config/wal/templates/"
+create_symlink "$CURRENT_DIRECTORY/scripts" "~/.local/bin/colorscript"
+create_symlink "$CURRENT_DIRECTORY/scripts/custwall" "~/.local/bin/custwall"
+create_symlink "$CURRENT_DIRECTORY/swaylock" "~/.config/swaylock"
 
-ln -s $CURRENT_DIRECTORY/hypr ~/.config/hypr/
-
-ln -s $CURRENT_DIRECTORY/i3 ~/.config/i3/
-
-ln -s $CURRENT_DIRECTORY/liferea ~/.config/liferea/ 
-
-ln -s $CURRENT_DIRECTORY/neofetch ~/.config/neofetch 
-
-ln -s $CURRENT_DIRECTORY/nvim ~/.config/nvim/ 
-
-ln -s $CURRENT_DIRECTORY/polybar ~/.config/polybar/ 
-
-ln -s $CURRENT_DIRECTORY/qBittorrent ~/.config/qBittorrent/ 
-
-ln -s $CURRENT_DIRECTORY/rofi ~/.config/rofi/ 
-
-ln -s $CURRENT_DIRECTORY/starship/starship.toml ~/.config/starship.toml
-
-ln -s $CURRENT_DIRECTORY/waybar ~/.config/waybar/ 
-
-ln -s $CURRENT_DIRECTORY/wlogout ~/.config/wlogout/ 
-
-ln -s $CURRENT_DIRECTORY/scripts/chalac ~/.local/bin/chalac
-
-ln -s $CURRENT_DIRECTORY/scripts/chchrome ~/.local/bin/chchrome
-
-ln -s $CURRENT_DIRECTORY/scripts/chwal ~/.local/bin/chwal
-
-ln -s $CURRENT_DIRECTORY/scripts/getwall ~/.local/bin/getwall
-
-ln -s $CURRENT_DIRECTORY/scripts/gtk.sh ~/.local/bin/gtk.sh
-
-ln -s $CURRENT_DIRECTORY/scripts/rofiwifimenu ~/.local/bin/rofiwifimenu
-
-ln -s $CURRENT_DIRECTORY/scripts/waybar-progress ~/.local/bin/waybar-progress 
-
-ln -s $CURRENT_DIRECTORY/pywaltemplates ~/.config/wal/templates/ 
-
-ln -s $CURRENT_DIRECTORY/scripts ~/.local/bin/colorscript
-
-ln -s $CURRENT_DIRECTORY/scripts/custwall /home/adnan/.local/bin/custwall
-
-ln -s $CURRENT_DIRECTORY/swaylock ~/.config/swaylock  
-
+echo "Installation complete."
