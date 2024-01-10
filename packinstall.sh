fb5c55dace5b6dba2f1d3740612173f25f4d7abb
Adnan-Malik-26 <eddymalikadnan@gmail.com>
2024-01-11 02:24:10 +0530

    script fixes

--- packinstall.sh
+++ packinstall.sh
@@ -1,7 +1,7 @@
 sudo pacman -Syu adobe-source-code-pro-fonts adwaita-cursors adwaita-icon-theme alsa archlinux-keyring autoconf automake awesome-terminal-fonts bat binutils bluez cmake gcc git discord duf gping fish electron python313 extra-cmake-modules fakeroot feh ffmpeg fontconfig gawk gnome-calculator gnome-tweaks go grub i3-wm imagemagick man-db neofetch neovim sddm networkmanager zsh alacritty base-devel bandwhich blueman coreutils diff-so-fancy curl tldr thunar dunst figlet gpaste lsd lxappearance make mesa ninja npm okular picom polybar qbittorrent virt-manager qemu ranger rofi ttf-jetbrains-mono ttf-firacode-nerd bat starship ttf-ms-fonts noto-fonts-emoji libreoffice ventoy openrgb obs-studio swayidle github-cli swayidle xorg-fonts-100dpi xorg-fonts-75dpi mako meson nerd-fonts-git openssh perl polkit ppsspp telegram-desktop themix-full-git
 
 mkdir ~/Clones/
-cd Clones 
+cd ~/Clones 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si 
