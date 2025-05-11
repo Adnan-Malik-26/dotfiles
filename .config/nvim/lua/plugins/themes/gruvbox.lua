c4667501e50adf547ecd9c1fffdb5b2b80e74bb3
Adnan-Malik-26 <eddymalikadnan@gmail.com>
2025-05-12 00:16:44 +0530

    a

--- .config/nvim/lua/plugins/themes/gruvbox.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'ellisonleao/gruvbox.nvim',
-  priority = 1000,
-  config = true,
-  opts = {
-    transparent_mode = true,
-  },
-}
--- .config/waybar/launch.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-killall -9 waybar
-waybar &
